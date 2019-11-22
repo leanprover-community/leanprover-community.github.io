50d4e63dfd24b40fb340c627ed2b8d7721e9362d
Rob Lewis <rob.y.lewis@gmail.com>
2019-11-22 13:49:10 +0100

    add mathlib docs

---
+++ mathlib_docs/nav.js
@@ -0,0 +1,58 @@
+var expanded = [];
+
+function unexpand(name) {
+    expanded = expanded.filter(function(e) {return e != name;});
+    sessionStorage.setItem('expanded', expanded.join(','));
+}
+
+function getExpandedCookie() {
+    var v = sessionStorage.getItem("expanded");
+    if (v) {
+        expanded = v.split(',').filter(function(e){return e != "";});
+    } else {
+        expanded = [];
+    }
+}
+
+function showItem(item) {
+    item.className = "nav_sect_inner";
+    expanded.push(item.id);
+    sessionStorage.setItem("expanded", expanded.join(","));
+}
+
+function hideItem(item) {
+    //item.style.display = "none";
+    item.className = "nav_sect_inner hidden";
+    unexpand(item.id);
+}
+
+function expandExpanded() {
+    for (var i = 0; i < expanded.length; i++) {
+        document.getElementById(expanded[i]).className = "nav_sect_inner";
+    }
+}
+
+function showNav(path) {
+    var items = path.split("/");
+    var i;
+    for (i = 0; i < items.length - 1; i++) {
+        d = document.getElementById(items.slice(0, i + 1).join("/"));
+        showItem(d);
+    }
+}
+
+var coll = document.getElementsByClassName("nav_sect");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        var content = this.nextElementSibling;
+        if (content.className === "nav_sect_inner") {
+            hideItem(content);
+        } else {
+            showItem(content);
+        }
+    });
+}
+
+getExpandedCookie();
+expandExpanded();
