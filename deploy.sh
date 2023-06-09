5c490d710fbb99fcac62d969ed066906af9377f2
Scott Morrison <scott.morrison@anu.edu.au>
2023-06-09 16:05:44 +1000

    chore: update branch name for lean3 website

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git clone --branch master "https://$DEPLOY_GITHUB_USER:$DEPLOY_GITHUB_TOKEN@gith
 
 ./make_site.py
 
-if [ "$github_repo" = "leanprover-community/leanprover-community.github.io" -a "$github_ref" = "refs/heads/newsite" ]; then
+if [ "$github_repo" = "leanprover-community/leanprover-community.github.io" -a "$github_ref" = "refs/heads/lean3" ]; then
   cd build/
   git config user.email "leanprover.community@gmail.com"
   git config user.name "leanprover-community-bot"
