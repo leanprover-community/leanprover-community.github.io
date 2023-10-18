5496e3ef421b3bdd02a4649baf804386fc05945b
Rob Lewis <rob.y.lewis@gmail.com>
2023-10-18 18:18:15 -0400

    move courses page to teaching directory; add redirect from old one

--- make_site.py
+++ make_site.py
@@ -446,7 +446,7 @@ def render_site(target: Path, base_url: str, reloader=False):
                 ('mathlib_stats.html', {'num_defns': num_defns, 'num_thms': num_thms, 'num_contrib': num_contrib}),
                 ('lean_projects.html', {'projects': projects}),
                 ('events.html', {'old_events': old_events, 'new_events': new_events}),
-                ('courses.html', {'courses': courses, 'tags': courses_tags}),
+                ('teaching/courses.html', {'courses': courses, 'tags': courses_tags}),
                 ('teams.html', {'introduction': (DATA/'teams_intro.md').read_text(encoding='utf-8'), 'teams': teams}),
                 ('.*.md', get_contents)
                 ],
