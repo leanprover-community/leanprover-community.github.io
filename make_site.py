80b2a1658d215d1a081e66d42b5b9c6f241af495
Rob Lewis <rob.y.lewis@gmail.com>
2024-01-05 11:20:14 -0500

    use zulip data in map

--- make_site.py
+++ make_site.py
@@ -22,6 +22,8 @@ import os
 from github import Github
 from slugify import slugify
 
+import zulip
+
 class MarkdownExtension(jinja2.ext.Extension):
     tags = set(['markdown'])
 
@@ -455,13 +457,48 @@ class User:
     fullname: str
     lon: float
     lat: float
-    zulip: str
     github: Optional[str] = None
     website: Optional[str] = None
 
-with (DATA/'users.yaml').open('r', encoding='utf-8') as h_file:
-    users = [User(**e) for e in yaml.safe_load(h_file)]
+client = zulip.Client(email='map-scraper-bot@leanprover.zulipchat.com', site='https://leanprover.zulipchat.com', api_key=os.environ.get('ZULIP_KEY'))
 
+# Zulip custom profile fields are tracked by ID, not by name
+profile_data_fields = {
+    'website': '5150',
+    'latitude': '5148',
+    'longitude': '5149',
+    'github': '3658'
+}
+
+def get_user_field(user, field):
+    try:
+        return user['profile_data'][profile_data_fields[field]]['value']
+    except KeyError:
+        return None
+
+def validate_coordinates(user):
+    try:
+        float(get_user_field(user, 'latitude'))
+        float(get_user_field(user, 'longitude'))
+        return True
+    except Exception:
+        return False
+
+
+zulip_users = [user for user in client.get_members({"include_custom_profile_fields": True})['members'] 
+               if not user['is_bot'] and user['is_active'] and validate_coordinates(user)] if 'ZULIP_KEY' in os.environ else []
+
+users = [
+    User(
+        fullname=user['full_name'], 
+        lon=float(get_user_field(user, 'longitude')), 
+        lat=float(get_user_field(user, 'latitude')), 
+        github=get_user_field(user, 'github'),
+        website=get_user_field(user, 'website')) 
+    for user in zulip_users]
+
+# for testing purposes
+print(f'user count: {len(users)}')
 
 def render_site(target: Path, base_url: str, reloader=False):
     default_context = lambda: {
