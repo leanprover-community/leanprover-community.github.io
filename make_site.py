92c3fccd6ab1c13e819bbd469d96a33062c534e5
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-01-10 11:24:07 -0500

    change logic for which theorems count as formalized (#574)
    (and move out of templates into make_site.py)
    
    cf.
    [Zulip](https://leanprover.zulipchat.com/#narrow/channel/113488-general/topic/1000-plus.20theorems.20project/near/492814764)
    
    ---------
    
    Co-authored-by: grunweg <rothgang@math.uni-bonn.de>

--- make_site.py
+++ make_site.py
@@ -247,6 +247,8 @@ class TheoremForWebpage:
     """
     id: str
     title: str
+    # A boolean tagging theorems that have been formalized
+    formalized: bool
     # The HTML source code for the generated documentation entries
     # for the declaration associated to this theorem.
     doc_decls: Optional[List[DocDecl]]
@@ -362,7 +364,9 @@ def download_N_theorems(kind: NTheorems) -> dict:
                             # note: the `header-data.json` data file uses doc-relative links
                             docs_link='/mathlib4_docs/' + decl_info.info.docLink,
                             src_link=decl_info.info.sourceLink))
-                theorems.append(TheoremForWebpage(id, h.title, doc_decls, links, h.author, h.date, note))
+                # A theorem counts as formalized if the author field or `doc_decls` is non-empty.
+                formalized = bool(h.author) or (len(doc_decls) > 0)
+                theorems.append(TheoremForWebpage(id, h.title, formalized, doc_decls, links, h.author, h.date, note))
         pkl_dump(name, theorems)
     else:
         theorems = pkl_load(name, dict())
