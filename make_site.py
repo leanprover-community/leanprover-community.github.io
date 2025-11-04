2fbd9d61ce9168ccabcdf3587ddba855fa3e2279
Anne C.A. Baanen <vierkantor@vierkantor.com>
2025-11-04 18:58:19 +0100

    Add tooltip for the documentation tags.

--- make_site.py
+++ make_site.py
@@ -533,7 +533,7 @@ for course in courses:
             setattr(course, field, render_markdown("\n".join(map(lambda v: "* " + v, val))))
 courses_tags = ['lean4', 'lean3'] + sorted(list(courses_tags))
 
-documentation_tags = set()
+documentation_tags = {}
 documentation_lists = {
         'tutorial': [],
         'how-to': [],
@@ -541,10 +541,13 @@ documentation_lists = {
         'reference': [],
 }
 with (DATA/'documentation.yaml').open('r', encoding='utf-8') as file:
-    for e in yaml.safe_load(file):
+    docu_data = yaml.safe_load(file)
+    for e in docu_data["tags"]:
+        documentation_tags[e["name"]] = e["description"]
+
+    for e in docu_data["documentation"]:
         e = DocumentationEntry(**e)
         documentation_lists[e.category].append(e)
-        documentation_tags.update(e.tags)
 
 # Cannot use %-d format code on windows
 def format_month_day(date_obj):
@@ -905,7 +908,7 @@ def render_site(target: Path, base_url: str, reloader=False, only: Optional[str]
                 ('events.html', {'old_events': old_events, 'new_events': new_events}),
                 ('teaching/courses.html', {'courses': courses, 'tags': courses_tags}),
                 ('teams.html', {'introduction': read_md('teams_intro.md'), 'teams': teams}),
-                ('documentation.html', {'documentation_lists': documentation_lists, 'documentation_tags': sorted(documentation_tags)}),
+                ('documentation.html', {'documentation_lists': documentation_lists, 'documentation_tags': documentation_tags}),
                 ('.*.md', get_contents)
                 ],
             filters={ 'url': url, 'md': render_markdown, 'tex': clean_tex },
